5ac63a5299b11a7edf0e719738c920ce6956193b
Your Name <skorokhodov.vs@phystech.edu>
2021-10-05 15:13:24 +0300

    add first working version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 It is designed to help students use python to
 make calculations and count errors in their laboratory works
+
+to download just paste in command line:
+```bash
+$ pip install --extra-index-url https://testpypi.python.org/pypi GrannyLab
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+setup(
+    name="GrannyLab",
+    version="0.0.2",
+    author="sevashasla",
+    author_email="skorokhodov.vs@phystech.edu",
+    url="https://github.com/sevashasla/GrannyLab",
+    long_description=read("README.md"),
+    description="library for easy errors cointing",
+    packages=find_packages(where="src"),
+    package_dir={'': 'src'},
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy>=1.17.4",
+    ],
+
+)
